26ee06292b9311a9b848bb38a972b5801ae1734f
Eza Nanda <37768282+21Ezza@users.noreply.github.com>
2020-07-20 20:24:12 +0700

    Add files via upload

---
+++ classtest.py
@@ -0,0 +1,181 @@
+import numpy as np #linear algebra
+import pandas as pd #data processing , CSV file I/O (Baca file dataset)
+import os
+import csv
+
+#Import libraries and cookbooks
+
+from IPython.display import HTML ## Setting display options for Ipython Notebook
+import pandas as pd
+import numpy as np
+from scipy import sparse
+from lightfm import LightFM
+from sklearn.metrics.pairwise import cosine_similarity
+
+# print(os.listdir("input")) #cek file input
+
+# ratings = pd.read_csv("input/ratings.csv") 
+# ratings = ratings[:1000]
+# ratings.shape
+# ratings.head(12)
+
+class training:
+    def __init__(self, ratings, movie):
+        csvfile = open('input/ratings.csv','rb')
+        csvfileM = open('input/movie.csv','rb')    
+        self.ratings = csv.render(csvfile)
+
+        self.movie = csv.render(csvfileM)
+
+    def create_interaction_matrix(self, df, user_col, item_col, rating_col, norm= False, threshold = None):
+        '''
+        Function to create an interaction matrix dataframe from transactional type interactions
+        Required Input -
+            - df = Pandas DataFrame containing user-item interactions
+            - user_col = column name containing user's identifier
+            - item_col = column name containing item's identifier
+            - rating col = column name containing user feedback on interaction with a given item
+            - norm (optional) = True if a normalization of ratings is needed
+            - threshold (required if norm = True) = value above which the rating is favorable
+        Expected output - 
+            - Pandas dataframe with user-item interactions ready to be fed in a recommendation algorithm
+        '''
+        
+        interactions = df.groupby([user_col, item_col])[rating_col] \
+                .sum().unstack().reset_index(). \
+                fillna(0).set_index(user_col)
+        
+        if norm:
+            interactions = interactions.applymap(lambda x: 1 if x > threshold else 0)
+        return interactions
+    
+    def create_user_dict(self, interactions):
+        '''
+        Function to create a user dictionary based on their index and number in interaction dataset
+        Required Input - 
+            interactions - dataset create by create_interaction_matrix
+        Expected Output -
+            user_dict - Dictionary type output containing interaction_index as key and user_id as value
+        '''
+        user_id = list(interactions.index)
+        user_dict = {}
+        counter = 0 
+        for i in user_id:
+            user_dict[i] = counter
+            counter += 1
+        return user_dict
+
+    def create_item_dict(self, df, id_col, name_col):
+        '''
+        Function to create an item dictionary based on their item_id and item name
+        Required Input - 
+            - df = Pandas dataframe with Item information
+            - id_col = Column name containing unique identifier for an item
+            - name_col = Column name containing name of the item
+        Expected Output -
+            item_dict = Dictionary type output containing item_id as key and item_name as value
+        '''
+        item_dict ={}
+        for i in range(df.shape[0]):
+            item_dict[(df.loc[i,id_col])] = df.loc[i,name_col]
+        return item_dict
+    
+    def runMF(self, interactions, n_components=30, loss='warp', k=15, epoch=30,n_jobs = 4):
+        '''
+        Function to run matrix-factorization algorithm
+        Required Input -
+            - interactions = dataset create by create_interaction_matrix
+            - n_components = number of embeddings you want to create to define Item and user
+            - loss = loss function other options are logistic, brp
+            - epoch = number of epochs to run 
+            - n_jobs = number of cores used for execution 
+        Expected Output  -
+            Model - Trained model
+        '''
+        x = sparse.csr_matrix(interactions.values)
+        model = LightFM(no_components= n_components, loss=loss,k=k)
+        model.fit(x,epochs=epoch,num_threads = n_jobs)
+        return model
+    
+    
+    def sample_recommendation_user(self, model, interactions, user_id, user_dict, 
+                                item_dict,threshold = 0,nrec_items = 10, show = True):
+        '''
+        Function to produce user recommendations
+        Required Input - 
+            - model = Trained matrix factorization model
+            - interactions = dataset used for training the model
+            - user_id = user ID for which we need to generate recommendation
+            - user_dict = Dictionary type input containing interaction_index as key and user_id as value
+            - item_dict = Dictionary type input containing item_id as key and item_name as value
+            - threshold = value above which the rating is favorable in new interaction matrix
+            - nrec_items = Number of output recommendation needed
+        Expected Output - 
+            - Prints list of items the given user has already bought
+            - Prints list of N recommended items  which user hopefully will be interested in
+        '''
+        n_users, n_items = interactions.shape
+        user_x = user_dict[user_id]
+        scores = pd.Series(model.predict(user_x,np.arange(n_items)))
+        scores.index = interactions.columns
+        scores = list(pd.Series(scores.sort_values(ascending=False).index))
+        
+        known_items = list(pd.Series(interactions.loc[user_id,:] \
+                                    [interactions.loc[user_id,:] > threshold].index) \
+                                    .sort_values(ascending=False))
+        
+        scores = [x for x in scores if x not in known_items]
+        return_score_list = scores[0:nrec_items]
+        known_items = list(pd.Series(known_items).apply(lambda x: item_dict[x]))
+        scores = list(pd.Series(return_score_list).apply(lambda x: item_dict[x]))
+        if show == True:
+            print("Known Likes:")
+            counter = 1
+            for i in known_items:
+                print(str(counter) + '- ' + i)
+                counter+=1
+
+            print("\n Recommended Items:")
+            counter = 1
+            for i in scores:
+                print(str(counter) + '- ' + i)
+                counter+=1
+        return return_score_list
+    
+    def run(self, path):
+        # Creating interaction matrix using rating data
+        interactions = self.create_interaction_matrix(df = ratings,
+                                                user_col = 'userId',
+                                                item_col = 'movieId',
+                                                rating_col = 'rating')
+        interactions.head(10)
+        
+        movies = pd.read_csv(path)
+        movies.head()
+
+        # Create User Dict
+        user_dict = self.create_user_dict(interactions=interactions)
+        # Create Item dict
+        movies_dict = self.create_item_dict(df = movies,
+                                    id_col = 'movieId',
+                                    name_col = 'title')
+        
+        mf_model = self.runMF(interactions = interactions,
+                        n_components = 30,
+                        loss = 'warp',
+                        epoch = 30,
+                        n_jobs = 4)
+        
+        ## Calling 10 movie recommendation for user id 7
+        rec_list = self.sample_recommendation_user(model = mf_model, 
+                                            interactions = interactions, 
+                                            user_id = 7, 
+                                            user_dict = user_dict,
+                                            item_dict = movies_dict, 
+                                            threshold = 4,
+                                            nrec_items = 10,
+                                            show = True)
+        
+        
+train = training()
+train.run(movie);
